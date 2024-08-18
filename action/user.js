a3f737049e90c503450335fed49f42f1b3535164
Deepak <hdeepak5902@gmail.com>
2024-08-18 16:05:39 +0530

    making changes

--- action/user.js
+++ action/user.js
@@ -1,10 +1,29 @@
-const register = async(formData)=>{
-    const firstName = formData.get('firstname')
-    const lastName = formData.get('lastname')
-    const email = formData.get('email')
-    const password = formData.get('password')
-    console.log(firstName, lastName, email, password)
-    
-}
+"use server"
 
-export default {register}
+import connectDB from "@/lib/db";
+import { User } from "@/models/User";
+import { redirect } from "next/navigation";
+import {hash} from "bcryptjs"
+
+export const register = async (formData) => {
+    const firstName = formData.get('firstname');
+    const lastName = formData.get('lastname');
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+
+    if(!firstName || !lastName || !email || !password){
+        throw new Error("Fill all the fields")
+    }
+
+    await connectDB()
+
+    const existingUser = await User.findOne({email});
+    
+    if(existingUser) throw new Error("User Already Exist")
+
+    const hashedPassword = await hash(password, 12)
+
+    await User.create({firstName, lastName, email, password : hashedPassword})
+    redirect('/login')
+};
--- auth.js
+++ auth.js
@@ -1,5 +1,40 @@
-import NextAuth from "next-auth"
+import NextAuth, { CredentialsSignin } from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { User } from "./models/User"
+import connectDB from "./lib/db"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      name: 'Credentails',
+
+      credentials:{
+        email: {label: "Email", type: "email"},
+        password: {label: "Password", type: "passwrod" },
+      },
+
+      authorize: async(credentials)=>{
+        const email = credentials.email
+        const password = credentials.password
+
+      if(!email || !password){
+        throw new CredentialsSignin("Please provide email and password")
+
+      }
+
+      await connectDB()
+
+      const user  = await User.findOne({email}).select("password +role")
+
+      if(!user){
+        throw new Error("Invalid Password or Email")
+      }
+
+      if(!user.password){
+        throw new Error
+      }
+
+      }
+    })
+  ],
 })
